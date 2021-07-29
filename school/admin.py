18467b0ef5aacccdf823951d53f957611fd8b868
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-29 15:03:48 -0400

    models and migrations

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student, Course
+
+admin.site.register(Student)
+admin.site.register(Course)
+
 
 # Register your models here.
--- school/models.py
+++ school/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    credits = models.IntegerField()
+
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=255)
+    courses = models.ManyToManyField(related_name='students')
