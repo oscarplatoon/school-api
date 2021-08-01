5e66950a03ca18d854bfc2868c3be4c942c80d2d
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-08-01 12:38:08 -0500

    Shawn's school api solution

--- school_app/serializers.py
+++ school_app/serializers.py
@@ -2,15 +2,15 @@ from rest_framework import serializers
 from .models import Student
 from .models import Course
 
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = ['id', 'name', 'age', 'courses']
-        depth = 1
-
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ['id', 'title', 'description', 'credits', 'students']
+        fields = ['id', 'title', 'description', 'credits']
         depth = 1
         
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id', 'name', 'age']
+        depth = 1
