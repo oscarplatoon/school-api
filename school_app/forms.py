95175fea75da0adfae554d29ff82c1cef15e8dd5
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-08-01 12:33:01 -0500

    Shawn's heroku solution school api v1.2

--- school_app/forms.py
+++ school_app/forms.py
@@ -5,12 +5,11 @@ from .models import Student, Course
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ('name', 'age')
+        fields = ('name', 'age', 'courses')
 
 
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ('title', 'description', 'credits', 'student')
+        fields = ('title', 'description', 'credits')
 
-        
